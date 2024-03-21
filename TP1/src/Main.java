a2e0233d8794103ffbe174578a5e2ecdf05f1b7d
yatel-espasandin <yatel.espasandin@gmail.com>
2024-03-21 15:47:30 -0300

    Switch y metodo manejo ArrayList agregado

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,18 +1,41 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
     Scanner dataEntry = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Scanner dataEntry = new Scanner(System.in);
         Main m =  new Main();
+        char control;
+        do {
+            System.out.println("Test de ejercicios a continuacion: ");
+            System.out.println(">Ingrese 1 para ingresar numeros enteros y promediarlos.");
+            System.out.println(">Ingrese 2 para obtener los primeros 100 numeros primos.");
+            System.out.println(">Ingrese 3 para obtener codigo ASCII de un caracter.");
 
-        int[] arrayProm = new int[5];
-        int promedio = m.promArr(arrayProm);
-        System.out.printf("El promedio de los numeros ingresados es: " + promedio);
-
-        int[] arrayPrimo = new int[100];
-        controlPrimo(arrayPrimo);
+            int controlS = dataEntry.nextInt();
+            switch(controlS) {
+                case 1:
+                    int[] arrayProm = new int[5];
+                    int promedio = m.promArr(arrayProm);
+                    System.out.println("El promedio de los numeros ingresados es: " + promedio);
+                break;
+                case 2:
+                    int[] arrayPrimo = new int[100];
+                    controlPrimo(arrayPrimo);
+                break;
+                case 3:
+                    System.out.println("Ingrese un caracter: ");
+                    char convertir = dataEntry.next().charAt(0);
+                    valorChar(convertir);
+                break;
+                case 4:
+                    ArrayList<Double> temperaturas = new ArrayList<>();
+                break;
+            }
+            System.out.println(">Desea continuar? (s/n)");
+            control = dataEntry.next().charAt(0);
+        }while(control == 's' || control == 'S');
     }
 
     public int promArr(int[] arr){
@@ -60,9 +83,21 @@ public class Main {
     }
 
     public static void printArray(int[] arr, int dim){
-        System.out.printf("Los primeros 100 numeros primos son: ");
+        System.out.println("Los primeros 100 numeros primos son: ");
         for(int i = 0; i < dim; i++){
-            System.out.print(arr[i] + " ");
+            System.out.println(arr[i] + " " );
         }
     }
+
+    public static void valorChar(char caracter){
+        System.out.println("El numero ASCII del caracter seleccionado es: " + (int)caracter);
+    }
+
+    public static void agregar(ArrayList<Double> arrList){
+        Scanner dataEntry = new Scanner(System.in);
+        System.out.println(">Ingrese una temperatura en C°: ");
+        Double tempAgregar = dataEntry.nextDouble();
+        arrList.add(tempAgregar);
+    }
+
 }
